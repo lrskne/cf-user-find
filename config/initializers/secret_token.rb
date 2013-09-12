098a42ad668d44a352732884b62c337e92ea27cf
Matt Smith <matt@nearapogee.com>
2013-09-12 16:01:28 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfUserFind::Application.config.secret_key_base = 'acce360b08348bd6e85a346c888149700c44b3d46bbe5a9ed59247cb35595bad7ed74b098e047b64280878811f382beca6d75bb66cdc6da0d06b9ff53d27eff8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CfUserFind::Application.config.session_store :cookie_store, key: '_cf-user-find_session'
