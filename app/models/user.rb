2631ffaae07a314f2de1b1ea3b774008957c4815
Laura Erskine <lrskne@gmail.com>
2013-09-15 18:13:07 -0700

    Add index on email, require name on User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :email, format: { with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "So sorry, your email format is invalid" }
+
+  validates :name, presence: true
 end
